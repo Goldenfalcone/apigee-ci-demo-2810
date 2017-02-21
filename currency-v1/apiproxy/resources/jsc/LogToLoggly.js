7b898e421ec3ee365eae8eeb8d9fd4f77bec7cd5
Jeremy <Goldenfalcone@users.noreply.github.com>
2017-02-21 16:11:49 -0600

    Fixed ' with "

--- currency-v1/apiproxy/resources/jsc/LogToLoggly.js
+++ currency-v1/apiproxy/resources/jsc/LogToLoggly.js
@@ -1,22 +1,22 @@
-var logglyUrlScheme = context.getVariable('flow.apigee.originalRequest.header.X-Forwarded-Proto');
-var logglyUrlDomain = context.getVariable('flow.apigee.originalRequest.header.Host');
-var logglyBasePath = context.getVariable('proxy.basepath');
+var logglyUrlScheme = context.getVariable("flow.apigee.originalRequest.header.X-Forwarded-Proto");
+var logglyUrlDomain = context.getVariable("flow.apigee.originalRequest.header.Host");
+var logglyBasePath = context.getVariable("proxy.basepath");
 var logglyUrl = logglyUrlScheme + '://' + logglyUrlDomain + logglyBasePath + '/logs';
-context.setVariable('debug.logglyUrl', logglyUrl);
+context.setVariable("debug.logglyUrl", logglyUrl);
 
 var log = 
-	org: context.getVariable('organization.name'),
-	env: context.getVariable('environment.name'),
-	api: context.getVariable('apiproxy.name'),
+	org: context.getVariable("organization.name"),
+	env: context.getVariable("environment.name"),
+	api: context.getVariable("apiproxy.name"),
 	request: {
-		resource: context.getVariable('proxy.pathsuffix'),
-		query: context.getVariable('flow.apigee.originalRequest.querystring'),
-		verb: context.getVariable('flow.apigee.originalRequest.verb')
-	};
+		resource: context.getVariable("proxy.pathsuffix"),
+		query: context.getVariable("flow.apigee.originalRequest.querystring"),
+		verb: context.getVariable("flow.apigee.originalRequest.verb")
+	},
 	error: {
-		status: context.getVariable('flow.apigee.error.status'),
-		code: context.getVariable('flow.apigee.error.code'),
-		message: context.getVariable('flow.apigee.error.message')
+		status: context.getVariable("flow.apigee.error.status"),
+		code: context.getVariable("flow.apigee.error.code"),
+		message: context.getVariable("flow.apigee.error.message")
 	}
 };
 
